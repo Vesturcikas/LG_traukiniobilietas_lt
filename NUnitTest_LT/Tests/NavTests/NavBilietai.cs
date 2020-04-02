a3f5c7bb2b19aff1a713f7e332368706bde02bc3
Vesturcikas <vestas1976@gmail.com>
2020-04-02 14:42:11 +0300

    added NavBilietai Tests

---
+++ NUnitTest_LT/Tests/NavTests/NavBilietai.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+using NUnit.Framework;
+
+namespace NUnitTest_LT.Tests.NavTests
+{
+    class NavBilietai : BaseTest
+    {
+        private IWebElement topNav => driver.FindElement(By.CssSelector(".nav"));
+        private IWebElement bilietaiNav => topNav.FindElement(By.LinkText("Bilietai"));
+
+        [SetUp]
+        public void BeforeTests()
+        {
+            string testUrl = baseUrl;
+            driver.Navigate().GoToUrl(testUrl);
+        }
+
+        [Test]
+        public void LgTopNavBilietai()
+        {
+
+            bilietaiNav.Click();
+            Assert.AreEqual("https://www.traukiniobilietas.lt/portal/", driver.Url);
+        }
+
+        [TearDown]
+        public void AfterTests()
+        {
+
+        }
+    }
+}
