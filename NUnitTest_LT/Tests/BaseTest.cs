0c3ba89057314990bd81b77979d192647777feaf
Vesturcikas <vestas1976@gmail.com>
2020-04-02 14:11:10 +0300

    added BaseTest class

---
+++ NUnitTest_LT/Tests/BaseTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+
+namespace NUnitTest_LT.Tests
+{
+    public class BaseTest
+    {
+        protected IWebDriver driver;
+        protected WebDriverWait wait;
+        protected Actions act;
+        protected string baseUrl;
+
+        [SetUp]
+        public void BeforeEveryTest()
+        {
+            baseUrl = "https://www.traukiniobilietas.lt/portal/";
+            var chromeOptions = new ChromeOptions();
+            chromeOptions.PageLoadStrategy = PageLoadStrategy.Eager;
+            driver = new ChromeDriver(chromeOptions);
+            driver.Manage().Window.Maximize();
+
+            //Setting up the Implicit Wait
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+
+            //setting up the wait
+            wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            //setting up the action
+            act = new Actions(driver);
+        }
+
+        [TearDown]
+        public void AfterEveryTest()
+        {
+            driver.Quit();
+            driver.Dispose();
+        }
+    }
+}
