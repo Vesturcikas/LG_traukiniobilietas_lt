7b84d3d58edf64bd93f4b6f65c62853f602f00c9
Vesturcikas <vestas1976@gmail.com>
2020-04-06 16:21:06 +0300

    added RegistrationPage sketch

---
+++ NUnitTest_LT/Pages/RegistrationPage.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace NUnitTest_LT.Pages
+{
+    public class RegistrationPage : BasePage
+    {
+        public RegistrationPage(IWebDriver driver) : base(driver) { }
+
+        public RegistrationPage InputCardNumber()
+        {
+            return this;
+        }
+
+        public RegistrationPage SelectCardDay()
+        {
+            return this;
+        }
+
+        public RegistrationPage SelectCardMonth()
+        {
+            return this;
+        }
+
+        public RegistrationPage SelectCardYear()
+        {
+            return this;
+        }
+
+        public RegistrationPage ClickCheck()
+        {
+            return this;
+        }
+
+        public RegistrationPage InputName()
+        {
+            return this;
+        }
+
+        public RegistrationPage InputSurname()
+        {
+            return this;
+        }
+
+        public RegistrationPage InputEmail()
+        {
+            return this;
+        }
+
+        public RegistrationPage InputStreet()
+        {
+            return this;
+        }
+        public RegistrationPage InputCity()
+        {
+            return this;
+        }
+
+        public RegistrationPage InputPhone()
+        {
+            return this;
+        }
+
+        public RegistrationPage InputPassword()
+        {
+            return this;
+        }
+
+        public RegistrationPage InputRepeatPassword()
+        {
+            return this;
+        }
+
+        public RegistrationPage ClickAgree()
+        {
+            return this;
+        }
+
+        public RegistrationPage ClickDisagree()
+        {
+            return this;
+        }
+
+        public RegistrationPage ClickSaveData()
+        {
+            return this;
+        }
+    }
+}
