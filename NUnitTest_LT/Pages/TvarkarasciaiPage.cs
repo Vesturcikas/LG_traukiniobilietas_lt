19e75ad74c0c84246e6be5238af99da80b5bf7f8
Vesturcikas <vestas1976@gmail.com>
2020-04-06 13:08:15 +0300

    added TvarkarasciaiPage sketch

---
+++ NUnitTest_LT/Pages/TvarkarasciaiPage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NUnitTest_LT.Pages
+{
+    public class TvarkarasciaiPage : BasePage
+    {
+
+        public TvarkarasciaiPage(IWebDriver drive) : base(drive) { }
+
+        public TvarkarasciaiPage ClickTimetable()
+        {
+            return this;
+        }
+
+        public TvarkarasciaiPage ClickSchedules()
+        {
+            return this;
+        }
+
+        public TvarkarasciaiPage SelectFrom()
+        {
+            return this;
+        }
+
+        public TvarkarasciaiPage SelectTo()
+        {
+            return this;
+        }
+
+        public TvarkarasciaiPage ClickSearch()
+        {
+            return this;
+        }
+
+
+    }
+}
