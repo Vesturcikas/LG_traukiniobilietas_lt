d3de3e87e0cea20ce313fbd6202dfc4c812c8740
Vesturcikas <vestas1976@gmail.com>
2020-04-22 11:56:50 +0300

    changed Services

---
+++ NUnitTest_LT/Tests/Services/ServicesTests.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using NUnit.Framework;
+using NUnit.Allure.Core;
+
+namespace NUnitTest_LT.Tests.Services
+{
+    [AllureNUnit]
+    public class ServicesTests : BaseTest
+    {
+        private IWebElement topNavbar => driver.FindElement(By.CssSelector(".nav"));
+        private IWebElement servicesNavbar => topNavbar.FindElement(By.CssSelector(".top-business"));
+        private IWebElement servicesSubMenu => servicesNavbar.FindElement(By.CssSelector(".submenu-wrap"));
+
+        [SetUp]
+        public void BeforeTests()
+        {
+            string testUrl = baseUrl;
+            driver.Navigate().GoToUrl(testUrl);           
+        }
+
+        [Test]
+        public void TopNavPaslaugos()
+        {
+            servicesNavbar.Click();
+            Assert.IsTrue(servicesSubMenu.Enabled);
+        }
+
+        [TearDown]
+        public void AfterTests()
+        {
+
+        }
+    }
+}
